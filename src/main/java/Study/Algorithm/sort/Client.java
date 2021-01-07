d9e887a70f9d15e9b441e77ca310318c370d2314
phoniex-fly <shixiaoyan@sinosoft.com.cn>
2021-01-07 17:00:01 +0800

    希尔排序

--- src/main/java/Study/Algorithm/sort/Client.java
+++ src/main/java/Study/Algorithm/sort/Client.java
@@ -2,14 +2,14 @@ package Study.Algorithm.sort;
 
 import java.util.Arrays;
 
-import Study.Algorithm.sort.mergeSort.MergeSort;
+import Study.Algorithm.sort.insertionSort.ShellSort;
 
 public class Client {
 	public static void main(String[] args) {
 		// 初始化数组
-		int[] array = { 6, 57, 72, 22 };
+		int[] array = { 6, 57, 72, 22, 4, 77, 5, 39 };
 		// 排序
-		Sort sort = new MergeSort();
+		Sort sort = new ShellSort();
 		sort.sort(array);
 		// 输出结果
 		System.out.println(Arrays.toString(array));
