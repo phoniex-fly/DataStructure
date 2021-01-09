ccac5907251261ffebd7e85dc2fa1f0685d1b63a
phoniex-fly <shixiaoyan@sinosoft.com.cn>
2021-01-09 19:21:20 +0800

    计数排序优化

--- src/main/java/Study/Algorithm/sort/Client.java
+++ src/main/java/Study/Algorithm/sort/Client.java
@@ -2,14 +2,14 @@ package Study.Algorithm.sort;
 
 import java.util.Arrays;
 
-import Study.Algorithm.sort.uncmp.CountingSort;
+import Study.Algorithm.sort.uncmp.CountingSort2;
 
 public class Client {
 	public static void main(String[] args) {
 		// 初始化数组
-		int[] array = { 6, 57, 72, 22, 4, 77, 5, 39 };
+		int[] array = { 6, 6, 12, 13, 8, 7, 10, 11 };
 		// 排序
-		Sort sort = new CountingSort();
+		Sort sort = new CountingSort2();
 		sort.sort(array);
 		// 输出结果
 		System.out.println(Arrays.toString(array));
